3e3e4581dce37ae4c30e9ee966d47fe73c480ecd
Yuan Ning <ningyuan215@gmail.com>
2019-02-24 00:33:54 -0500

    upload

---
+++ Array/238. Product of Array Except Self.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        p = 1
+        for i in range(len(nums)):
+            res.append(p)
+            p = p * nums[i]
+        
+        q = 1
+        for i in range(len(nums)-1,-1, -1):
+            res[i] *= q
+            q = q*nums[i]
+        return res
+        
